a9511ae42e12865d82c0851943a8d34fd9083834
atahtee <atatisam14@gmail.com>
2023-08-07 21:24:47 +0300

    minor changes on getpost

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,9 +4,9 @@ import PostMessage from "../models/postMessage.js"
 export const getPosts =async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
-        res.status(200).json(postMessages);
+         return res.status(200).json(postMessages);
     } catch (error) {
-        res.status(404).json({message: error.message});
+        return res.status(404).json({message: error.message});
     }
 }
 
