49b4b4c621bacddaf080efe51d2008da87796d9a
atahtee <atatisam14@gmail.com>
2023-08-07 20:17:15 +0300

    Cors error fixed

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
+app.use(cors({
+    origin: "https://frame-it-djm7.onrender.com",
+  }));
 
 app.use('/posts', postRoutes)
 
@@ -19,7 +21,7 @@ const CONNECTION_URL = "mongodb+srv://atati:ZcvdrTYbnO12rBTd@inkonnect.mlk6fnz.m
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server inakimbia on port: ${PORT}`)))
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message) );
 
 
